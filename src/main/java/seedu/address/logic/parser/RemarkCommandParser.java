2c921146453f2c41820e654865c50cabf9297d82
J-hta-n <e0969121@u.nus.edu>
2023-10-02 12:43:19 +0800

    Add Remark Command
    Add Remark Command is based on the tp tutorial from cs2103-2324s1's
    github website in order to better understand how the existing code
    base for the AB3 works. The summary of changes is as follows:
    
    Firstly, we trace through the program and see that the entry point
    for executing user commmands is via ui.MainWindow::executeCommand,
    which gets the LogicManager object to obtain a CommandResult, which
    is then shown the user as feedback.
    
    For the remark command, LogicManager::execute parses the user input
    using AddressBookParser::parseCommand, which makes use of
    RemarkCommandParser::parse to return a valid RemarkCommand object,
    before RemarkCommand::execute is called which updates the person in
    the model with the new remark.
    
    Next, LogicManager::execute saves the updated model into the hard
    disk, before finally returning the CommandResult object, which is
    passed back to the MainWindow object to show the feedback to the
    user.

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.parser;
+// RemarkCommandParser.java taken from tutorial at https://nus-cs2103-ay2324s1.github.io/tp/tutorials/AddRemark.html and
+// from https://github.com/se-edu/addressbook-level3/commit/dc6d5139d08f6403da0ec624ea32bd79a2ae0cbf#diff-8bf239e8e9529369b577701303ddd96af93178b4ed6735f91c2d8488b20c6b4a
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
+
+/**
+ * Parses input arguments and creates a new {@code RemarkCommand} object
+ */
+public class RemarkCommandParser implements Parser<RemarkCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code RemarkCommand}
+     * and returns a {@code RemarkCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public RemarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        Remark remark = new Remark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
+
+        return new RemarkCommand(index, remark);
+    }
+}
